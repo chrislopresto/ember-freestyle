0f1ae3ca154a64c83ec3cc5d39897f3da18d26d4
Andrew Fan <andrew.fan@upsilonit.com>
2016-02-25 12:19:53 +0300

    add bower dependencies to parent project

---
+++ blueprints/ember-freestyle/index.js
@@ -0,0 +1,15 @@
+module.exports = {
+
+  name: 'ember-freestyle',
+
+  normalizeEntityName: function() {},
+
+  afterInstall: function() {
+    var bowerPackages = [
+      { name: 'remarkable', target: '1.6.2' },
+      { name: 'highlightjs', target: '9.1.0' }
+    ];
+
+    return this.addBowerPackagesToProject(bowerPackages);
+  }
+};
