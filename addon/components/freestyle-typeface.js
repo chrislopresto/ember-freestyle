bf65537ee334804c8834b1adc00688c09c09163b
Chris LoPresto <chris@chrislopresto.com>
2016-03-15 23:19:51 -0400

    Add freestyle-typeface component

---
+++ addon/components/freestyle-typeface.js
@@ -0,0 +1,15 @@
+import Ember from 'ember';
+import layout from '../templates/components/freestyle-typeface';
+
+const { computed } = Ember;
+const { htmlSafe } = Ember.String;
+
+export default Ember.Component.extend({
+  layout,
+  classNames: ['FreestyleTypeface'],
+  attributeBindings: ['fontFamilyStyle:style'],
+
+  fontFamilyStyle: computed('fontFamily', function() {
+    return htmlSafe(`font-family: "${this.get('fontFamily')}";`);
+  })
+});
