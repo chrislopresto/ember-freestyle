2e30f0ee18218484e236b2a548e6c1daf7263f0b
Eric Polino <epolino@vikus.com>
2016-04-13 15:47:59 -0400

    inject was unused

--- addon/components/freestyle-palette.js
+++ addon/components/freestyle-palette.js
@@ -1,15 +1,15 @@
 import Ember from 'ember';
 import layout from '../templates/components/freestyle-palette';
 
-const { computed, inject } = Ember;
+const { A, computed, Component } = Ember;
 
-export default Ember.Component.extend({
+export default Component.extend({
   tagName: '',
   layout,
   title: 'Freestyle Palette',
 
   colors: computed('colorPalette', function() {
-    return Ember.A(
+    return A(
       Object.keys(this.get('colorPalette')).map((k) => {
         return this.get('colorPalette')[k];
       })
