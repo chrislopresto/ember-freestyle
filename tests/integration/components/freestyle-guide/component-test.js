f2c084e7512bfa4f50d0bd3bfc2d7c09e14862d8
Chris LoPresto <chris@chrislopresto.com>
2017-04-30 18:07:09 -0400

    Wire up control of highlight.js theme (#104)
    Wire up control of the highlight.js theme on a freestyle-guide component or a freestyle-usage component.

---
+++ tests/integration/components/freestyle-guide/component-test.js
@@ -0,0 +1,39 @@
+import { moduleForComponent, test } from 'ember-qunit';
+import hbs from 'htmlbars-inline-precompile';
+import usage from '../../../pages/usage-component';
+import Ember from 'ember';
+
+// Stub freestyle service
+const FreestyleStub = Ember.Service.extend({
+  defaultTheme: 'solarized-light'
+});
+
+moduleForComponent('freestyle-guide', 'Integration | Component | freestyle guide', {
+  integration: true,
+
+  beforeEach() {
+    this.register('service:emberFreestyle', FreestyleStub);
+    this.inject.service('emberFreestyle');
+    usage.setContext(this);
+  },
+
+  afterEach() {
+    usage.removeContext();
+  }
+});
+
+test('it sets the passed in highlightJsTheme as the default theme on the service', function(assert) {
+  assert.expect(2);
+
+  assert.equal(this.get('emberFreestyle.defaultTheme'), 'solarized-light');
+  this.render(hbs`
+    {{#freestyle-guide
+        title='Ember Freestyle'
+        subtitle='Living Style Guide'
+        highlightJsTheme='zenburn'
+    }}
+      I am the guide
+    {{/freestyle-guide}}
+    `);
+  assert.equal(this.get('emberFreestyle.defaultTheme'), 'zenburn');
+});
