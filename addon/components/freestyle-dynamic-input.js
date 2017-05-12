d130d155e039b256dfa9d801d4494f910730b49f
Dan Wenzel <dwenzel@pagerduty.com>
2017-05-12 11:25:17 -0500

    move equality to JS, since no more ember-truth-helpers

--- addon/components/freestyle-dynamic-input.js
+++ addon/components/freestyle-dynamic-input.js
@@ -1,11 +1,14 @@
 import Ember from 'ember';
 import layout from '../templates/components/freestyle-dynamic-input';
 
-const { computed } = Ember;
+const { computed, computed: { equal } } = Ember;
 
 export default Ember.Component.extend({
   layout,
   classNames: ['FreestyleDynamic-input'],
+
+  isCheckbox: equal('inputType', 'checkbox'),
+  isTextarea: equal('inputType', 'textarea'),
 
   inputId: computed('propertyName',  function () {
     return `${this.get('elementId')}_${this.get('propertyName')}`;
