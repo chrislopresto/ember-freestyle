135bddd748a5972dedf047528015c04a8a5ff6e2
Dan Wenzel <dwenzel@pagerduty.com>
2017-05-13 11:23:49 -0500

    Call freestyle-usage inside freestyle-dynamic

--- addon/components/freestyle-dynamic.js
+++ addon/components/freestyle-dynamic.js
@@ -4,4 +4,6 @@ import layout from '../templates/components/freestyle-dynamic';
 export default Ember.Component.extend({
   layout,
   classNames: ['FreestyleDynamic'],
+}).reopenClass({
+  positionalParams: ['slug']
 });
