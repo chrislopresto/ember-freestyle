d67346a95c5f07a47482066a11f588bad771dabc
Chris LoPresto <chris.lopresto@betterment.com>
2017-07-14 16:26:44 -0400

    Update locationType for production docs site

--- tests/dummy/config/environment.js
+++ tests/dummy/config/environment.js
@@ -41,5 +41,9 @@ module.exports = function(environment) {
     ENV.APP.rootElement = '#ember-testing';
   }
 
+  if (environment === 'production') {
+    ENV.locationType = 'hash';
+  }
+
   return ENV;
 };
