81a628d85fc8373f1825c5f9bc7099210b3cc0c9
Camille TJHOA <camille.tjhoa@outlook.com>
2017-12-08 16:57:56 +0000

    Reenable tests on ember 2.4

--- config/ember-try.js
+++ config/ember-try.js
@@ -3,6 +3,22 @@ module.exports = {
   useYarn: true,
   scenarios: [
     {
+      name: 'ember-lts-2.4',
+      bower: {
+        dependencies: {
+          'ember': 'components/ember#lts-2-4'
+        },
+        resolutions: {
+          'ember': 'lts-2-4'
+        }
+      },
+      npm: {
+        devDependencies: {
+          'ember-source': null
+        }
+      }
+    },
+    {
       name: 'ember-lts-2.8',
       bower: {
         dependencies: {
