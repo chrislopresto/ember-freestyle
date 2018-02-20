520ac3b6043ba19df271a3680b49f251876571d2
Sivakumar Kailasam <sivakumar.ur.friend@gmail.com>
2018-02-20 21:28:34 +0530

    Addressed review comments

--- config/ember-try.js
+++ config/ember-try.js
@@ -1,9 +1,44 @@
 module.exports = {
+  useYarn: true,
   scenarios: [
+    {
+      name: 'ember-lts-2.4',
+      bower: {
+        dependencies: {
+          'ember': 'components/ember#lts-2-4'
+        },
+        resolutions: {
+          'ember': 'lts-2-4'
+        }
+      },
+      npm: {
+        devDependencies: {
+          'ember-source': null
+        }
+      }
+    },
+    {
+      name: 'ember-lts-2.8',
+      bower: {
+        dependencies: {
+          'ember': 'components/ember#lts-2-8'
+        },
+        resolutions: {
+          'ember': 'lts-2-8'
+        }
+      },
+      npm: {
+        devDependencies: {
+          'ember-native-dom-event-dispatcher': null,
+          'ember-source': null
+        }
+      }
+    },
     {
       name: 'ember-lts-2.12',
       npm: {
         devDependencies: {
+          'ember-native-dom-event-dispatcher': null,
           'ember-source': '~2.12.0'
         }
       }
@@ -12,7 +47,17 @@ module.exports = {
       name: 'ember-lts-2.16',
       npm: {
         devDependencies: {
+          'ember-native-dom-event-dispatcher': null,
           'ember-source': '~2.16.0'
+        }
+      }
+    },
+    {
+      name: 'ember-lts-2.18',
+      npm: {
+        devDependencies: {
+          'ember-native-dom-event-dispatcher': null,
+          'ember-source': '~2.18.0'
         }
       }
     },
