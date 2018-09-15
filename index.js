0e531f3ed08b5c1544bbbbbdbeb2f4ed140e158d
Luke Melia <luke@lukemelia.com>
2018-09-14 23:05:22 -0400

    Update our SnippetFinder plugin to use broccoli-plugin and update related deps

--- index.js
+++ index.js
@@ -1,22 +1,18 @@
 'use strict';
-var mergeTrees = require('broccoli-merge-trees');
-var fs   = require('fs');
-var flatiron = require('broccoli-flatiron');
-var freestyleUsageSnippetFinder = require('./freestyle-usage-snippet-finder');
+const mergeTrees = require('broccoli-merge-trees');
+const fs = require('fs');
+const flatiron = require('broccoli-flatiron');
+const FreestyleUsageSnippetFinder = require('./freestyle-usage-snippet-finder');
 
 module.exports = {
   name: 'ember-freestyle',
 
-  treeForApp: function(tree) {
-    var treesToMerge = [tree];
-    var self = this;
+  treeForApp(tree) {
+    let treesToMerge = [tree];
+    let snippets = mergeTrees(this.snippetPaths().filter((path) => fs.existsSync(path)));
 
-    var snippets = mergeTrees(this.snippetPaths().filter(function(path) {
-      return fs.existsSync(path);
-    }));
-
-    snippets = mergeTrees(this.snippetSearchPaths().map(function(path) {
-      return freestyleUsageSnippetFinder(path, self.ui);
+    snippets = mergeTrees(this.snippetSearchPaths().map((path) => {
+      return new FreestyleUsageSnippetFinder([path], this.ui);
     }).concat(snippets));
 
     snippets = flatiron(snippets, {
@@ -27,27 +23,27 @@ module.exports = {
     return mergeTrees(treesToMerge);
   },
 
-  snippetPaths: function() {
+  snippetPaths() {
     if (this.app) {
-      var freestyleOptions = this.app.options.freestyle || {};
+      let freestyleOptions = this.app.options.freestyle || {};
       return freestyleOptions.snippetPaths || ['snippets'];
     }
     return ['snippets'];
   },
 
-  snippetSearchPaths: function() {
+  snippetSearchPaths() {
     if (this.app) {
-      var freestyleOptions = this.app.options.freestyle || {};
+      let freestyleOptions = this.app.options.freestyle || {};
       return freestyleOptions.snippetSearchPaths || ['app'];
     }
     return ['app'];
   },
 
-  included: function(/*app, parentAddon*/) {
+  included(/*app, parentAddon*/) {
     this._super.included.apply(this, arguments);
   },
 
-  isDevelopingAddon: function() {
+  isDevelopingAddon() {
     return false;
   }
 };
