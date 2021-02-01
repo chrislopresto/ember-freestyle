9870924d68e6017ad9b3dfa935cf8157480a526d
Matt McManus <matt@mcmanus.io>
2021-02-01 17:11:57 -0500

    Arg.Array support

---
+++ addon/components/freestyle/usage/array/index.js
@@ -0,0 +1,32 @@
+import Component from '@glimmer/component';
+import { action } from '@ember/object';
+import { equal } from 'macro-decorators';
+
+const STRING = 'String';
+const BOOL = 'Boolean';
+const NUMBER = 'Number';
+const OBJECT = 'Object';
+export default class FreestyleUsageStringComponent extends Component {
+  @action addItem() {
+    let { onChange, items } = this.args;
+    items.push('');
+    onChange(items);
+  }
+
+  @action updateItem(index, value) {
+    let { onChange, items } = this.args;
+    items[index] = value;
+    onChange(items);
+  }
+
+  @action removeItem(index) {
+    let { onChange, items } = this.args;
+    items.splice(index, 1);
+    onChange(items);
+  }
+
+  @equal('args.type', STRING) useStringControl;
+  @equal('args.type', BOOL) useBooleanControl;
+  @equal('args.type', NUMBER) useNumberControl;
+  @equal('args.type', OBJECT) useObjectControl;
+}
