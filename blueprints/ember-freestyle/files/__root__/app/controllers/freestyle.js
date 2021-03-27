61525f74e76fe5d4ab3c4f9fdb151be96927e326
Bert De Block <bert.deblock@bagaar.be>
2021-03-27 12:43:00 +0100

    chore: clean up blueprint files

--- blueprints/ember-freestyle/files/__root__/app/controllers/freestyle.js
+++ blueprints/ember-freestyle/files/__root__/app/controllers/freestyle.js
@@ -1,60 +1,29 @@
-import Ember from 'ember';
 import BaseFreestyleController from 'ember-freestyle/controllers/freestyle';
 import { inject as service } from '@ember/service';
 
 export default class FreestyleController extends BaseFreestyleController {
   @service emberFreestyle;
 
-  /* BEGIN-FREESTYLE-USAGE fp--notes
-### A few notes regarding freestyle-palette
-
-- Accepts a colorPalette POJO like the one found in the freestyle.js blueprint controller
-- Looks very nice
-
-And another thing...
-
-###### Markdown note demonstrating prettified code
-
-```
-import Ember from 'ember';
-
-export default Ember.Component.extend({
-  // ...
-  colorPalette: {
-    'primary': {
-      'name': 'cyan',
-      'base': '#00bcd4'
-    },
-    'accent': {
-      'name': 'amber',
-      'base': '#ffc107'
-    }
-  }
-  // ...
-});
-```
-  END-FREESTYLE-USAGE */
-
   colorPalette = {
-    'primary': {
-      'name': 'cyan',
-      'base': '#00bcd4'
+    primary: {
+      name: 'cyan',
+      base: '#00bcd4',
     },
-    'accent': {
-      'name': 'amber',
-      'base': '#ffc107'
+    accent: {
+      name: 'amber',
+      base: '#ffc107',
     },
-    'secondary': {
-      'name': 'greyish',
-      'base': '#b6b6b6'
+    secondary: {
+      name: 'greyish',
+      base: '#b6b6b6',
     },
-    'foreground': {
-      'name': 'blackish',
-      'base': '#212121'
+    foreground: {
+      name: 'blackish',
+      base: '#212121',
     },
-    'background': {
-      'name': 'white',
-      'base': '#ffffff'
-    }
+    background: {
+      name: 'white',
+      base: '#ffffff',
+    },
   };
 }
