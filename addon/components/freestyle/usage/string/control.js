bab95a14843a1a39f000675176c9607db0e06925
Bert De Block <bert.deblock@bagaar.be>
2021-09-25 18:54:26 +0200

    refactor: remove custom `pick` helper

--- addon/components/freestyle/usage/string/control.js
+++ addon/components/freestyle/usage/string/control.js
@@ -1,7 +1,13 @@
+import { action } from '@ember/object';
 import Component from '@glimmer/component';
 
 export default class FreestyleUsageStringControlComponent extends Component {
   get inputEvent() {
     return this.args.inputEvent || 'input';
+  }
+
+  @action
+  callOnInput(event) {
+    this.args.onInput(event.target.value);
   }
 }
