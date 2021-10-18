197a248cacab95a79bdbbd7693c1abf84c7bac89
Bert De Block <bert.deblock@bagaar.be>
2021-10-18 16:16:54 +0200

    chore: remove `ember-cli-page-object`

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -1,12 +1,12 @@
 import Application from 'dummy/app';
 import config from 'dummy/config/environment';
-import { setApplication } from '@ember/test-helpers';
-import { start } from 'ember-qunit';
-import { useNativeEvents } from 'ember-cli-page-object/extend';
 import * as QUnit from 'qunit';
+import { setApplication } from '@ember/test-helpers';
 import { setup } from 'qunit-dom';
+import { start } from 'ember-qunit';
+
+setApplication(Application.create(config.APP));
 
 setup(QUnit.assert);
-setApplication(Application.create(config.APP));
-useNativeEvents();
+
 start();
